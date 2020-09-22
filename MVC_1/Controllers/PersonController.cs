066aadb839f99c4001f9c5dd287e3e45fcf462a7
Shjani49 <shjani49@gmail.com>
2020-09-22 12:18:49 -0600

    Create Page;

--- MVC_1/Controllers/PersonController.cs
+++ MVC_1/Controllers/PersonController.cs
@@ -9,20 +9,13 @@ namespace MVC_1.Controllers
 {
     public class PersonController : Controller
     {
-        // In Class Practice 2:
-        // Write a List View that will output the names of all the people in the AppPeople list.
-
-        // Use a cached list to store the data without a database.
-        // The data will not persist when the app closes.
-        // It must be static to persist through page loads.
-        static public List<Person> AppPeople { get; set; } = new List<Person>();
-
         public IActionResult Index()
         {
             return RedirectToAction("List");
         }
 
-        public IActionResult Create(string id, string firstName, string lastName, string phone)
+        // id is not necessary as it is now AUTO_INCREMENT in the database, and is generated thereby.
+        public IActionResult Create(string firstName, string lastName, string phone)
         {
             // When this Action gets called, there are 3 likely states:
             // 1) First page load, no data has been provided (initial state).
@@ -32,9 +25,12 @@ namespace MVC_1.Controllers
             // A request has come in that has some data stored in the query (GET or POST).
             if (Request.Query.Count > 0)
             {
-                if (id != null && firstName != null && lastName != null && phone != null)
+                if (firstName != null && lastName != null && phone != null)
                 {
                     // All expected data provided, so this will be our submit state.
+
+                    // Replace the list add with a context add.
+                    // Generate the new model instances to be added to the database.
                     Person newPerson = new Person()
                     {
                         FirstName = firstName,
@@ -45,6 +41,14 @@ namespace MVC_1.Controllers
                         Number = phone,
                         Person = newPerson
                     };
+                    // Add the new model instances to the database.
+                    using (PersonContext context = new PersonContext())
+                    {
+                        context.People.Add(newPerson);
+                        context.PhoneNumbers.Add(newPhoneNumber);
+                        context.SaveChanges();
+                    }
+
                     ViewBag.Success = "Successfully added the person to the list.";
 
                 }
@@ -54,7 +58,6 @@ namespace MVC_1.Controllers
                     ViewBag.Error = "Not all fields have had values provided.";
 
                     // Store our data to re-add to the form.
-                  
                     ViewBag.FirstName = firstName;
                     ViewBag.LastName = lastName;
                     ViewBag.Phone = phone;
@@ -68,7 +71,12 @@ namespace MVC_1.Controllers
 
         public IActionResult List()
         {
-            ViewBag.People = AppPeople;
+            // Just like with Create() all we have to do is translate our logic from List to Context.
+            using (PersonContext context = new PersonContext())
+            {
+                ViewBag.People = context.People.ToList();
+            }
+
             return View();
         }
 
@@ -88,14 +96,28 @@ namespace MVC_1.Controllers
 
             return result;
         }
+
         public Person GetPersonByID(int id)
         {
-            return AppPeople.Where(x => x.ID == id).Single();
+            Person target;
+            // We have to make a separate query for phone numbers, unless we use something called a DTO (Data Transfer Object) to bind them to.
+            // Due to time constraints that may or may not be covered, so this is a workaround.
+            List<PhoneNumber> phoneNumbers;
+            using (PersonContext context = new PersonContext())
+            {
+                target = context.People.Where(x => x.ID == id).Single();
+                phoneNumbers = context.PhoneNumbers.Where(x => x.PersonID == target.ID).ToList();
+            }
+            // When we initially query to get the "target", EF will only enumerate (retreive) the records from THAT table (by default).
+            // This is a workaround to make sure that the phone numbers are assigned to the object that gets returned.
+            // There are better and more efficient ways to do this, but this will serve our purposes and is easier to understand at face value.
+            target.PhoneNumbers = phoneNumbers;
+            return target;
         }
 
         public void DeletePersonByID(int id)
         {
-            AppPeople.Remove(AppPeople.Where(x => x.ID == id).Single());
+            // AppPeople.Remove(AppPeople.Where(x => x.ID == id).Single());
 
 
         }
