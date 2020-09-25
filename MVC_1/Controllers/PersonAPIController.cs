14fa729e436ebee6f89bbcf854d8570e9c279f11
Shjani49 <shjani49@gmail.com>
2020-09-25 11:29:00 -0600

    Split into 3 more organized files but maintained the 2 classes;

--- MVC_1/Controllers/PersonAPIController.cs
+++ MVC_1/Controllers/PersonAPIController.cs
@@ -1,27 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MVC_1.Models;
 using MVC_1.Models.Exceptions;
 
 namespace MVC_1.Controllers
 {
-    // In-Class Practice: 
-    // Add a PersonController method to get all people whose name starts with J.
-    // Add an API endpoint that will return the results of that method to "People/NamesStartingJ".
-
-    // Challenge:
-    // Modify the PersonController method to accept a character.
-    // Make the API endpoint respond to all possible starting characters at "People/StartsWith/{char}"
-    // Add and API endpoint at "People/ID/{id}" that will return only the person with that ID.
-
-    // In-Class Practice Part 2:
-    // Add a second endpoint for the "name starts with" that uses the query string and not the URL.
-
-
     // GET: Read / Query - Get some data.
     // POST: Create - Add some data.
     // PUT: Update (Overwrite) - Replace some data.
@@ -39,80 +24,59 @@ namespace MVC_1.Controllers
     // [Route("API/[controller]") and [HttpGet("People/Test")]
     // Our path should be: https://localhost:PORT/API/PersonAPI/People/Test
 
-    // This determines the first segment of the path. 
-    [Route("API/[controller]")]
     // This defines the controller as an API controller.
+    [Route("Person")]
     [ApiController]
     // Our class name (sans 'Controller') is substituted into [controller] in the Route annotation.
-    public class PersonAPIController : ControllerBase
+    public partial class PersonAPIController : ControllerBase
     {
         // This determines the second segment of the path.
-        [HttpGet("People/All")]
+        [HttpGet("API/All")]
 
         // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
-        public ActionResult<IEnumerable<Person>> GetAllPeople()
-        {
-            PersonController controller = new PersonController();
-            return controller.GetPeople();
-        }
-
-        // This determines the second segment of the path.
-        [HttpGet("People/MultiplePhones")]
-
-        // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
-        public ActionResult<IEnumerable<Person>> GetPeopleWithMultiplePhones()
+        public ActionResult<IEnumerable<Person>> AllPeople_GET()
         {
             // This is what we are returning. It gets serialized as JSON if we return an object.
-            PersonController controller = new PersonController();
-            return controller.GetPeopleWithMultiplePhoneNumbers();
-
-
+            return new PersonController().GetPeople();
         }
 
         // This determines the second segment of the path.
-        [HttpGet("People/StartsWith/{startChar:alpha}")]
+        [HttpGet("API/MultiplePhones")]
 
         // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
-        public ActionResult<IEnumerable<Person>> GetPeopleWhoseNamesStartWith(string startChar)
+        public ActionResult<IEnumerable<Person>> MultiplePhonesPeople_GET()
+        {
+            return new PersonController().GetPeopleWithMultiplePhoneNumbers();
+        }
+
+        // This determines the second segment of the path.
+        [HttpGet("API/StartsWith/{startChar:alpha}")]
+
+        // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
+        public ActionResult<IEnumerable<Person>> NameStartsWithPeople_GET_URL(string startChar)
         {
             // Assuming we aren't using the controller again, we might as well just instantiate it where we need it the one time.
             return new PersonController().GetPeopleStartingWith(startChar);
         }
 
         // This determines the second segment of the path.
-        [HttpGet("People/StartsWith")]
+        [HttpGet("API/StartsWith")]
 
         // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
-        public ActionResult<IEnumerable<Person>> GetPeopleWhoseNamesStartWithparameterized(string startChar)
+        public ActionResult<IEnumerable<Person>> NameStartsWithPeople_GET_QueryString(string startChar)
         {
             // Assuming we aren't using the controller again, we might as well just instantiate it where we need it the one time.
             return new PersonController().GetPeopleStartingWith(startChar);
         }
 
-
-
-        // This determines the second segment of the path.
-        //  [HttpGet("People/ID/{ID}")]
 
         // This method of GET parameters will retrieve the argument from the URL.
-        [HttpGet("People/ID/{id}")]
-
+        [HttpGet("API/ID/{id}")]
         // This is the return type of the request. The method name is irrelevant as far as the clients are concerned.
-        public ActionResult<object> GetPersonWithID(int id)
+        public ActionResult<object> PersonID_GET_URL(int id)
         {
-            /* // This is what we are returning. It gets serialized as JSON if we return an object.
-             Person person = new PersonController().GetPersonByID(id);
-
-             // This is "kind of" a DTO. We're putting the fields we care about into another object that is not the database model.
-             // They help get around errors like the circular references, and (if you use them in the context) the missing virtual properties.
-             return new
-             {
-                 id = person.ID,
-                 firstName = person.FirstName,
-                 lastName = person.LastName,
-                 phoneNumbers = person.PhoneNumbers.Select(x => x.Number)
-             };*/
             ActionResult<object> response;
+            // This is what we are returning. It gets serialized as JSON if we return an object.
             try
             {
                 Person person = new PersonController().GetPersonByID(id);
@@ -136,21 +100,9 @@ namespace MVC_1.Controllers
         }
 
         // This method of GET parameters will retrieve the argument from the query string (after the ? in the URL).
-        [HttpGet("People/ID")]
-        public ActionResult<object> GetPersonWithIDParameterized(int id)
+        [HttpGet("API/ID")]
+        public ActionResult<object> PersonID_GET_QueryString(int id)
         {
-            /* // This is what we are returning. It gets serialized as JSON if we return an object.
-             Person person = new PersonController().GetPersonByID(id);
-
-             // This is "kind of" a DTO. We're putting the fields we care about into another object that is not the database model.
-             // They help get around errors like the circular references, and (if you use them in the context) the missing virtual properties.
-             return new
-             {
-                 id = person.ID,
-                 firstName = person.FirstName,
-                 lastName = person.LastName,
-                 phoneNumbers = person.PhoneNumbers.Select(x => x.Number)
-             */
             ActionResult<object> response;
             // This is what we are returning. It gets serialized as JSON if we return an object.
             try
@@ -175,7 +127,6 @@ namespace MVC_1.Controllers
             return response;
         }
 
-
         // Patches can either be in this format below, where an endpoint does one action, or in a format that specifies the "instructions" in the query.
         /*
          For example:
@@ -189,17 +140,15 @@ namespace MVC_1.Controllers
           variable: Count,
           value: 5
         */
-        [HttpPatch("People/FirstName")]
-        public ActionResult ChangeFirstName(int id, string newName)
+        [HttpPatch("API/FirstName")]
+        public ActionResult ChangePersonFirstName_PATCH(int id, string newName)
         {
             ActionResult response;
             try
             {
                 new PersonController().ChangeFirstNameByID(id, newName);
-              
                 response = Ok(new { message = $"Successfully renamed person {id} to {newName}." });
             }
-        
             catch (InvalidOperationException)
             {
                 response = NotFound(new { error = $"The requested person at ID {id} does not exist." });
@@ -213,8 +162,8 @@ namespace MVC_1.Controllers
             return response;
         }
 
-        [HttpPost("People/Create")]
-        public ActionResult CreatePerson(string firstName, string lastName, string phone)
+        [HttpPost("API/Create")]
+        public ActionResult CreatePerson_POST(string firstName, string lastName, string phone)
         {
             ActionResult response;
             try
@@ -242,9 +191,38 @@ namespace MVC_1.Controllers
             return response;
         }
 
-       
-        [HttpDelete("People/Delete")]
-        public ActionResult DeletePerson(string id)
+        [HttpPut("API/Update")]
+        public ActionResult UpdatePerson_PUT(string id, string firstName, string lastName)
+        {
+            ActionResult response;
+            try
+            {
+                new PersonController().UpdatePerson(id, firstName, lastName);
+
+                // Semantically, we should be including a copy of the object (or at least a DTO rendering of it) in the Ok response.
+                // For our purposes, a message with the fields will suffice.
+                response = Ok(new { message = $"Successfully update person at ID {id} to be {firstName} {lastName}." });
+            }
+            catch (PersonValidationException e)
+            {
+                // If it couldn't find the entity to update, that's the primary concern, so discard the other subexceptions and just return NotFound().
+                if (e.SubExceptions.Any(x => x.GetType() == typeof(NullReferenceException)))
+                {
+                    response = NotFound(new { error = $"No entity exists at ID {id}." });
+                }
+                // If there's no NullReferenceException, but there's still an exception, return the list of problems.
+                else
+                {
+                    response = UnprocessableEntity(new { errors = e.SubExceptions.Select(x => x.Message) });
+                }
+            }
+
+
+            return response;
+        }
+
+        [HttpDelete("API/Delete")]
+        public ActionResult DeletePerson_DELETE(string id)
         {
             ActionResult response;
 
@@ -277,4 +255,3 @@ namespace MVC_1.Controllers
         }
     }
 }
-
--- MVC_1/Controllers/PersonControllerBLL.cs
+++ MVC_1/Controllers/PersonControllerBLL.cs
@@ -1,58 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using MVC_1.Models;
 using MVC_1.Models.Exceptions;
 
 namespace MVC_1.Controllers
 {
-
-    public class PersonController : Controller
+    public partial class PersonController : Controller
     {
-        // In-class practice:
-        // Add validation to ensure that the first and last name do not contain numbers.
-        // Add trimming to the CreatePerson method.
-        // Add validation that neither first nor last name are greater than 50 characters long.
 
-        public IActionResult Index()
-        {
-            return RedirectToAction("List");
-        }
-
-        // id is not necessary as it is now AUTO_INCREMENT in the database, and is generated thereby.
-        public IActionResult Create(string firstName, string lastName, string phone)
-        {
-            // When this Action gets called, there are 3 likely states:
-            // 1) First page load, no data has been provided (initial state).
-            // 2) Partial data has been provided (error state).
-            // 3) Complete data has been provided (submit state).
-
-            // A request has come in that has some data stored in the query (GET or POST).
-
-            if (Request.Query.Count > 0)
-            {
-                try
-                {
-                    CreatePerson(firstName, lastName, phone);
-
-                    ViewBag.Success = "Successfully added the person to the list.";
-                }
-                catch (PersonValidationException e)
-                {
-                    // All expected data not provided, so this will be our error state.
-                    ViewBag.Exception = e;
-
-                    // Store our data to re-add to the form.
-                    ViewBag.FirstName = firstName != null ? firstName.Trim() : null;
-                    ViewBag.LastName = lastName != null ? lastName.Trim() : null;
-                    ViewBag.Phone = phone != null ? phone.Trim() : null;
-                }
-            }
-
-            return View();
-        }
 
         // Do NOT do this in practices, have a separate create for phone numbers.
         // The only reason I did this is we scaffolded the phone number table and I didn't want to cause conflicts.
@@ -229,41 +187,7 @@ namespace MVC_1.Controllers
         }
 
 
-        public IActionResult List(string filter)
-        {
 
-            // Slightly different from practice as you'll be calling methods and not using a "using" in your action.
-            if (filter == "on")
-            {
-                ViewBag.People = GetPeopleWithMultiplePhoneNumbers();
-                ViewBag.Filter = filter;
-            }
-            else
-            {
-                ViewBag.People = GetPeople();
-            }
-
-
-
-            return View();
-        }
-
-        public IActionResult Details(string id, string delete)
-        {
-            IActionResult result;
-            if (delete != null)
-            {
-                DeletePersonByID(int.Parse(id));
-                result = RedirectToAction("List");
-            }
-            else
-            {
-                ViewBag.Person = GetPersonByID(int.Parse(id));
-                result = View();
-            }
-
-            return result;
-        }
 
         public List<Person> GetPeople()
         {
